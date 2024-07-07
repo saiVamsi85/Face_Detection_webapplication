5d3f419c963e5744bf25d58bbcfd7ce2e1365eb7
Sai Vamsi <146182219+saiVamsi85@users.noreply.github.com>
2024-07-07 12:03:17 +0530

    Updated face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -3,7 +3,7 @@ import cv2
 # Load the Haar Cascade Classifier for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Open the default camera (usually the built-in webcam, camera index 0)
+# Try to open the default camera (usually the built-in webcam, camera index 0)
 cap = cv2.VideoCapture(0)
 
 # Check if the camera opened successfully
